e14219f62d460e2a8252f0eb033ae39a062868e3
Nathan Baylon <nathanbaylon@gmail.com>
2026-06-07 03:11:40 +1000

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda i: i.start)
+
+        for i in range(1, len(intervals)):
+            interval1 = intervals[i-1]
+            interval2 = intervals[i]
+
+            if interval2.start < interval1.end:
+                return False
+
+        return True
