062aa137aafbefd0fea7bab90a6a40872863227d
Nathan Baylon <nathanbaylon@gmail.com>
2026-06-18 17:01:17 +1000

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var anagrams = new Dictionary<string, List<string>>();
+        var res = new List<List<string>>();
+
+        foreach(string str in strs) {
+            var key = new string(str.OrderBy(c => c).ToArray());
+
+            if (!anagrams.ContainsKey(key)) {
+                anagrams[key] = new List<string>();
+            }
+            anagrams[key].Add(str);
+        }
+
+        foreach(var kvp in anagrams) {
+            res.Add(kvp.Value);
+        }
+
+        return res;
+    }
+}
