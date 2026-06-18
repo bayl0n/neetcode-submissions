a39a63e94e967c9f03655151e04849b5fe5d6202
Nathan Baylon <nathanbaylon@gmail.com>
2026-06-19 02:12:49 +1000

    Add: lru-cache - submission-12

---
+++ Data Structures & Algorithms/lru-cache/submission-12.cs
@@ -0,0 +1,86 @@
+public class Node {
+    public int key;
+    public int value;
+    public Node next;
+    public Node previous;
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+}
+public class LRUCache {
+    private Node head;
+    private Node tail;
+    private int capacity;
+    private Dictionary<int, Node> nodes;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.nodes = new Dictionary<int, Node>();
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        head.next = tail;
+        tail.previous = head;
+    }
+
+    private void InsertList(Node node) {
+        Node previous = tail.previous;
+
+        previous.next = node;
+        tail.previous = node;
+
+        node.next = tail;
+        node.previous = previous;
+    }
+
+    private void DeleteList(Node node) {
+        if (node == tail) {
+            return;
+        }
+
+        Node curr = nodes[node.key];
+
+        Node previous = curr.previous;
+        Node next = curr.next;
+        previous.next = next;
+        next.previous = previous;
+    }
+    
+    public int Get(int key) {
+        if (!nodes.ContainsKey(key)) {
+            return -1;
+        }
+
+        Node curr = nodes[key];
+        DeleteList(curr);
+        InsertList(curr);
+
+        return nodes[key].value;
+    }
+    
+    public void Put(int key, int value) {
+        if (nodes.ContainsKey(key)) {
+            Node curr = nodes[key];
+            curr.value = value;
+
+            DeleteList(curr);
+            InsertList(curr);
+            nodes[key].value = value;
+            return;
+        }
+
+        Node newNode = new Node(key, value);
+
+        nodes.Add(key, newNode);
+        InsertList(newNode);
+
+        if (nodes.Count() > capacity) {
+            Node lru = head.next;
+            DeleteList(lru);
+            nodes.Remove(lru.key);
+        }
+    }
+}
