5be5f50ca85a778f0065a5a238322a545d2c940b
Nathan Baylon <nathanbaylon@gmail.com>
2026-06-26 12:52:31 +1000

    Add: dynamicArray - submission-1

---
+++ Data Structures & Algorithms/dynamicArray/submission-1.cs
@@ -0,0 +1,52 @@
+public class DynamicArray {
+    private int capacity;
+    private int size;
+    private int[] list;
+    
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.list = new int[capacity];
+    }
+
+    public int Get(int i) {
+        return list[i];
+    }
+
+    public void Set(int i, int n) {
+        list[i] = n;
+    }
+
+    public void PushBack(int n) {
+        size++;
+
+        if (size > capacity)
+            this.Resize();
+
+        list[size - 1] = n;
+    }
+
+    public int PopBack() {
+        var value = list[size-1];
+        size--;
+        return value;
+    }
+
+    private void Resize() {
+        capacity = capacity * 2;
+        var newList = new int[capacity];
+
+        foreach (var (index, value) in list.Index())
+            newList[index] = list[index];
+
+        list = newList;
+    }
+
+    public int GetSize() {
+        return size;
+    }
+
+    public int GetCapacity() {
+        return capacity;
+    }
+}
