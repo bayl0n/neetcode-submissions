050568a8a83a30b15e294445df4740b20655a308
Nathan Baylon <nathanbaylon@gmail.com>
2026-07-16 00:24:27 +1000

    Add: trapping-rain-water - submission-3

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-3.py
@@ -0,0 +1,44 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        """
+        elevation:
+            it is the min height for i and j
+
+        so for each elevation, we want to scan between i and j
+
+        we know water can be trapped if for a given height, it is less than the max_elevation
+        for i and j
+
+        - we want to track the max elevation between and i j
+        - when a new max elevation is found, we want to find the water heights between i and j for heights
+            and add them to our count
+        - if a height taller than our max elevation is found, we stop scanning and move our pointer to it
+        - when deciding to move i or j, since we want to find a new max elevation, we want to
+            move whichever side is smallest
+        """
+
+        max_elevation = 0
+        left = 0
+        water = 0
+        right = len(height) - 1
+
+        while left < right:
+            current_elevation = min(height[left], height[right])
+            max_elevation = max(max_elevation, current_elevation)
+
+            if height[left] > height[right]:
+                scan = right-1
+                while height[scan] < max_elevation and scan > left:
+                    water += (max_elevation - height[scan])
+                    scan -= 1
+
+                right = scan
+            else:
+                scan = left+1
+                while height[scan] < max_elevation and scan < right:
+                    water += (max_elevation - height[scan])
+                    scan += 1
+
+                left = scan
+
+        return water
